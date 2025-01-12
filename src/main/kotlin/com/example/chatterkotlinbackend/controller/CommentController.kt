5feffcae880b102908a4f4a30aa20f6770cb6a7a
Daniel <danielo.visage@gmail.com>
2025-01-12 11:06:46 +0100

    add comment creation and read

--- src/main/kotlin/com/example/chatterkotlinbackend/controller/CommentController.kt
+++ src/main/kotlin/com/example/chatterkotlinbackend/controller/CommentController.kt
@@ -2,6 +2,7 @@ package com.example.chatterkotlinbackend.controller
 
 import com.example.chatterkotlinbackend.model.Comment
 import com.example.chatterkotlinbackend.model.Post
+import com.example.chatterkotlinbackend.model.User
 import com.example.chatterkotlinbackend.repository.CommentRepository
 import com.example.chatterkotlinbackend.repository.PostRepository
 import com.example.chatterkotlinbackend.repository.UserRepository
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
 import java.util.*
 
 @RestController
@@ -27,15 +29,41 @@ class CommentController {
     @PostMapping
     fun createComment(
         @RequestBody comment: Comment,
-        @RequestParam user_id: String
+        @RequestParam user_id: String,
+        @RequestParam postId: String,
+    ): ResponseEntity<Comment> {
+        return try{
+            val author: Optional<User> = userRepository.findById(user_id)
+            val post: Optional<Post> = postRepository.findById(postId)
+            if(author.isEmpty){
+                return ResponseEntity(null, HttpStatus.BAD_REQUEST)
+            }
+            comment.author = author.get()
+            comment.post = post.get()
+            comment.createdAt = LocalDateTime.now()
+            val newComment: Comment = commentRepository.save(comment)
+            ResponseEntity(newComment, HttpStatus.CREATED)
+        } catch (e: RuntimeException) {
+            println("Error saving comment: ${e.message}")
+            ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
 
-    ){}
-
-    @GetMapping
+    @GetMapping("/{postId}")
     fun getCommentsByPost(
         @PathVariable("postId") postId: String
-    ): List<Comment>{
-        return commentRepository.findByPostId(postId)
+    ): ResponseEntity<List<Comment>>{
+        return try {
+            val comments: List<Comment> = commentRepository.findByPostId(postId)
+
+            if (comments.isEmpty()) {
+                ResponseEntity(HttpStatus.NO_CONTENT)
+            } else {
+                ResponseEntity(comments, HttpStatus.OK)
+            }
+        } catch(e: Exception) {
+            ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
 }
